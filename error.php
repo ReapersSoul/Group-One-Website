e3b22c9816444ed92e2fd18e6c8388a9a18f8faa
HollowShade <dakotagray13@yahoo.com>
2020-11-19 11:14:45 -0500

    Updated the team page, error page, and the SSI files
    I updated the pages I worked on (Team and Error) so they look as well as they did when they were still HTML files.  In addition, I added new feats to the team page, got both of my pages to use the header and footer SSI files, and updated said SSI files with the code we all share in the headers and footers of our pages.

--- error.php
+++ error.php
@@ -15,8 +15,8 @@
         <link rel="stylesheet" href="../css/bootstrap.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <!-- ours -->
-        <link rel="stylesheet" href="../Global.css">
-        <link rel="stylesheet" href="TeamError.css">
+        <link rel="stylesheet" href="css/Global.css">
+        <link rel="stylesheet" href="css/TeamError.css">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?     family=Lobster+Two:ital,wght@1,700&display=swap" rel="stylesheet">
@@ -25,18 +25,8 @@
 </head>
 
 <header>
-    <div class="col header fullWidth">
-        <div href="../Index.html" class="title">Pet Planet</div>
-        <nav class="row fullWidth linkCont" id="navbar">
-            <a href="../Index.html" class="link">Home</a>
-            <a href="../Marielle/Product.html" class="link">Shop</a>
-            <a href="../Trevor/Login.html" id="Login" class="link">Login</a>
-            <a href="../Trevor/Register.html" id="Register" class="link">Register</a>
-            <a href="../Marielle/About.html" class="link">About Us</a>
-            <a href="../Carter/Contact.html" class="link">Contact Us</a>
-            <a href="Team.html" class="link">Team</a>
-        </nav>
-    </div>
+    <!-- Link to Header.ssi-->
+    <?php include 'Header.ssi'; ?>
 </header>
 
 <body>
@@ -52,51 +42,14 @@
                 Please use the menu above, or the back button, to redirect yourself to another page.
                 </p>
                 <br>
-                <p style=color:darkgray>Alternatively, you can click the button below and it will redirect you to the home page.</p>
+              <p style=color:darkgray>Alternatively, you can click the button below and it will redirect you to the home page.</p>
                 <hr>
-            <button onclick="document.location='..//index.html'" class="button">Home</button>
+            <button onclick="document.location='index.php'" class="button">Home</button>
             </div>
         </div>
 </body>
 
-<!-- If only I knew more about the importance of the footer -->
-<footer>
-        <!-- jquery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <!-- bootstrap -->
-        <script src="../js/bootstrap.js"></script>
-        <!-- ours -->
-        <script src="../Global.js"></script>
-        <!-- popper and tippy -->
-        <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js"></script>
-        <script src="https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js"></script>
-
-        <script>
-          // With the above scripts loaded, you can call `tippy()` with a CSS
-          // selector and a `content` prop:
-          tippy('#Login', {
-            content: '<div id="invalid" class="Invalid"></div><form class="" action="Trevor/Login.php" method="post"><label for="Username">Username:</label><br><input type="text" name="Username" ><br><label for="Password">Password:</label><br><input type="password" name="Password" value=""><br><div class="row"><input type="submit" value="Login"></div></form>',
-            allowHTML: true,
-            interactive: true,
-          });
-
-          tippy('#Register', {
-            content: '<div id="invalid" class="Invalid"></div><form class="" action="Trevor/Register.php" method="post"><label for="Username">Username:</label><br><input type="text" name="Username" ><br><label for="Password">Password:</label><br><input type="password" name="Password" value=""><br><div class="row"><input type="submit" value="Register"></div></form>',
-            allowHTML: true,
-            interactive: true,
-          });
-        </script>
-
-        <script type="text/javascript">
-            if (getCookie("InvalidLogin")=="true") {
-                $('#invalid').append('Invalid Login!');
-                setCookie("InvalidLogin","");
-            }
-
-            if (getCookie("Username")!="") {
-                $('#navbar').append('<a href="UserPage.html" class="link">'+getCookie("Username")+'</a>');
-            }
-        </script>
-    </footer>
+    <!-- Link to Footer.ssi -->
+    <?php include 'Footer.ssi'; ?>
 
 </html>
