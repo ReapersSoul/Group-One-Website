f93b72471daf3641bbca38552117d91b1c2f5cea
ReapersSoul <56406337+ReapersSoul@users.noreply.github.com>
2020-11-28 18:46:15 -0500

    trevor finished

--- processlogin.php
+++ processlogin.php
@@ -3,35 +3,35 @@
     //echo $_POST["Username"]."<br>".$_POST["Password"]."<br>";
 
     $servername = "localhost";
-    $username = "admin";
+    $username = "root";
     $password = "";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password,"sql9365852");
+    $conn = new mysqli($servername, $username, $password,"useraccts");
 
     // Check connection
     if ($conn->connect_error) {
-      header('Location: ../Dakota/Error.html');
+      header('Location: ./Error.php');
     }
 
-    $sql = mysqli_real_escape_string ($conn, "SELECT * FROM Users WHERE UserName='".$_POST["Username"]."'");
-    $result = $conn->query($sql);
+    $Uname = mysqli_real_escape_string ($conn, $_POST["Username"]);
+    $result = $conn->query("SELECT * FROM Users WHERE UserName='".$Uname."'");
 
     if ($result->num_rows > 0) {
-      // output data of each row
       while($row = $result->fetch_assoc()) {
         if($row["UserName"]==$_POST["Username"] && $row["Password"]==$_POST["Password"]){
             //echo "id: " . $row["UserName"]. "<br>";
             setcookie("Username", $_POST["Username"],time() + 3600);
             setcookie("Password", $_POST["Password"],time() + 3600);
             setcookie("InvalidLogin", "false",time() + (3600/4));
+            $conn->close();
+            header('Location: ./member.php');
         }
       }
     } else {
         setcookie("InvalidLogin", "true",time() + (3600/4));
+        $conn->close();
+        echo "not valid";
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
-
-    $conn->close();
-
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
