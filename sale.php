0b1b957b42e8df75d40b099c2f3f2653fb7dbf6c
Marielle <benderm6@ferris.edu>
2020-12-03 11:57:28 -0500

    Update sale.php

--- sale.php
+++ sale.php
@@ -1,1 +1,51 @@
-<!-- marielle -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="This site is about a grocery store">
+    <meta name="keywords" content="Group 1 Store, Pet supply products">
+    <meta name="author" content="Marielle Bender">
+    <meta name="generator" content="Brackets">
+    <title>Pet Palace</title>
+      
+ <!--  =================== Insert the Favicon =========================== -->
+    <link rel="shortcut icon" href="favicon.ico"/>
+
+    <!--  =================== Bootstrap core CSS =========================== -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- ===================== Custom styles for this template ============ -->
+    <link href="css/Global.css" rel="stylesheet">
+   
+      
+      <!-- ===================== Fonts  ==================================== -->
+      <!-- Don't do both font methods. Choose one option -->
+      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+      
+    </head>
+        <body class="bdy">
+            <header>
+            <!-- Link to header-->
+            <?php include 'ssi/Header.ssi'; ?>
+            </header>
+    <main>
+        <?php
+        title= "Marivin Moose";
+        price= "$4.99"
+        descriptionTitle= "Description"
+        descriptionText= "The KONG Cozies are cute, soft and cuddly plush toys made with an extra layer of material, so they’re extra tough. Cozies are perfect for a game of fetch or as a comfort toy for your furry friend. Grab one of the 10 amazingly cute Cozie characters for your dog and we know your dog will love you for it."
+        BenefitsTitle= "Key Benefits"
+        BenefitsText= "Soft and cuddly plush toys made with an extra layer of material, so they’re extra tough.
+Minimal filling for less mess around the house.
+Perfect for a game of fetch or as a comfort toy for your furry friend.
+Squeaks to help entice play for extra excitement.
+Available in two different sizes: Small and Medium, so you can pick the perfect size for your pup."
+            </main>
+           
+            
+        
+            
+            
+    </body>
